ddc87702865754acf09c4f0cc88d121095d47052
Ognjen Radisic <radisicognjen@gmail.com>
2020-12-01 16:30:22 +0100

    increase decrease item count functionality

--- src/reducer.js
+++ src/reducer.js
@@ -6,6 +6,26 @@ const reducer = (state, action) => {
     const newItems = state.cart.filter((item) => item.id !== action.payload)
     return { ...state, cart: newItems }
   }
+  if (action.type === 'INCREASE') {
+    let newCart = state.cart.map((item) => {
+      if (item.id === action.payload) {
+        return { ...item, amount: item.amount + 1 }
+      }
+      return item
+    })
+    return { ...state, cart: newCart }
+  }
+  if (action.type === 'DECREASE') {
+    let newCart = state.cart
+      .map((item) => {
+        if (item.id === action.payload) {
+          return { ...item, amount: item.amount - 1 }
+        }
+        return item
+      })
+      .filter((item) => item.amount > 0)
+    return { ...state, cart: newCart }
+  }
 
   return state
 }
